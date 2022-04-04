2054644bd027f9eda4ec2307d2d1c84f549186e8
Daniel Rodas Bautista <daniel.bautista@tivix.com>
2022-04-04 19:47:44 -0400

    simplify main.js

--- django_web/vue/src/main.js
+++ django_web/vue/src/main.js
@@ -1,12 +1,14 @@
-import Vue from 'vue';
-import App from './App.vue';
-import router from './router';
-import store from './store';
+import Vue from 'vue/dist/vue'; // we need to specify Vue like this to have runtime compiler work with Vite
+
+// import App from './App.vue';
 
 Vue.config.productionTip = false;
+// Vue.component('alert-message', AlertMessage);
+// Vue.component('welcome-card', () => import('./components/WelcomeCard/WelcomeCard'));
 
 new Vue({
-  router,
-  store,
-  render: (h) => h(App),
-}).$mount('#app');
+  el: '#app',
+  mounted() {
+    console.log('Vue mounted');
+  },
+});
