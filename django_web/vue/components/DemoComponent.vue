eda259f3724e991d0e29371484cc83681aabc6c6
Daniel Rodas Bautista <daniel.bautista@tivix.com>
2022-04-04 20:22:17 -0400

    set up demo component

---
+++ django_web/vue/components/DemoComponent.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="bg-white">
+    <div class="max-w-7xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:px-8">
+      <div class="text-center">
+        <h2 class="text-base font-semibold text-indigo-600 tracking-wide uppercase">Demo</h2>
+        <p class="mt-1 text-4xl font-extrabold text-gray-900 sm:text-5xl sm:tracking-tight lg:text-6xl">{{title}}</p>
+        <p class="max-w-xl mt-5 mx-auto text-xl text-gray-500">Using Django, Vue and Vite together.</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DemoComponent',
+  props: {
+    title: {
+      type: String,
+      default: 'A fancy title',
+    },
+  },
+};
+</script>
--- django_web/vue/main.js
+++ django_web/vue/main.js
@@ -1,10 +1,7 @@
 import Vue from 'vue/dist/vue'; // we need to specify Vue like this to have runtime compiler work with Vite
 
-// import App from './App.vue';
-
 Vue.config.productionTip = false;
-// Vue.component('alert-message', AlertMessage);
-// Vue.component('welcome-card', () => import('./components/WelcomeCard/WelcomeCard'));
+Vue.component('demo-component', () => import('./components/DemoComponent'));
 
 new Vue({
   el: '#app',
